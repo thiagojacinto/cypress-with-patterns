99cc3894961e209e2cb7117f1b5b2fc6976e8452
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-10-02 18:24:10 -0300

    fix: importing Tarefa from index

--- cypress/integration/screenplay/adicionar/Tarefas/index.js
+++ cypress/integration/screenplay/adicionar/Tarefas/index.js
@@ -1,3 +1,4 @@
 export * from "./AcessarHome.tarefa";
 export * from "./InserirNovoRepositorio.tarefa";
 export * from "./ExibirLista.tarefa";
+export * from "./ExibirListaContendoTres.tarefa";
--- cypress/integration/screenplay/adicionar/adicionar.screenplay.spec.js
+++ cypress/integration/screenplay/adicionar/adicionar.screenplay.spec.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
 import { e, dado, quando, entao, Usuario } from "../../../support/screenplay";
-import { AcessarHome, ExibirLista, InserirNovoRepositorio } from "./Tarefas";
-import { ExibirListaContendoTres } from "./Tarefas/ExibirListaContendoTres.tarefa";
+import {
+  AcessarHome,
+  ExibirLista,
+  ExibirListaContendoTres,
+  InserirNovoRepositorio
+} from "./Tarefas";
 
 describe("Adicionar", () => {
   describe("Dado que esteja na home", () => {
